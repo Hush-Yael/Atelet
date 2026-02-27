b8fe5a698fc33f6831f5ecb16f9d1adea2934d04
Jean <srjean21@gmail.com>
2026-02-27 15:20:33 -0400

    feat: added some more style changes

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -21,6 +21,21 @@ class AppTheme {
         indicatorShape: const CircleBorder(),
         labelBehavior: .alwaysHide,
       ),
+
+      bottomAppBarTheme: t.bottomAppBarTheme.copyWith(height: 60),
+
+      bottomNavigationBarTheme: t.bottomNavigationBarTheme.copyWith(
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        elevation: 0,
+      ),
+
+      floatingActionButtonTheme: t.floatingActionButtonTheme.copyWith(
+        elevation: 0,
+        shape: const CircleBorder(),
+      ),
+
+      inputDecorationTheme: t.inputDecorationTheme.copyWith(filled: true),
     );
   }
 
@@ -30,10 +45,13 @@ class AppTheme {
     required Color primary,
     required Color onPrimary,
     required Color backgroundColor,
+    required Color surfaceColor,
     required Color textColor,
     required Color textDisplayColor,
     required Color dividerColor,
     required Color cardColor,
+    required Color inputFillColor,
+    required Color inputEnabledBorderColor,
   }) {
     return baseTheme.copyWith(
       colorScheme: baseTheme.colorScheme.copyWith(
@@ -48,8 +66,47 @@ class AppTheme {
         displayColor: textDisplayColor,
       ),
 
+      appBarTheme: baseTheme.appBarTheme.copyWith(
+        backgroundColor: surfaceColor,
+        foregroundColor: textColor,
+      ),
+
+      navigationBarTheme: baseTheme.navigationBarTheme.copyWith(
+        backgroundColor: surfaceColor,
+        indicatorColor: primary,
+        iconTheme: WidgetStateProperty.fromMap({
+          WidgetState.selected: IconThemeData(color: onPrimary),
+        }),
+      ),
+
+      bottomAppBarTheme: baseTheme.bottomAppBarTheme.copyWith(
+        color: surfaceColor,
+      ),
+
+      bottomNavigationBarTheme: baseTheme.bottomNavigationBarTheme.copyWith(
+        backgroundColor: surfaceColor,
+        selectedItemColor: primary,
+      ),
+
       dividerColor: dividerColor,
       cardColor: cardColor,
+
+      floatingActionButtonTheme: baseTheme.floatingActionButtonTheme.copyWith(
+        backgroundColor: primary,
+        foregroundColor: onPrimary,
+      ),
+
+      inputDecorationTheme: baseTheme.inputDecorationTheme.copyWith(
+        fillColor: inputFillColor,
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(width: 2.0),
+          borderRadius: BorderRadius.circular(8),
+        ).copyWith(borderSide: BorderSide(color: primary, width: 2.0)),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: inputEnabledBorderColor, width: 1.0),
+          borderRadius: BorderRadius.circular(8),
+        ),
+      ),
     );
   }
 
@@ -59,10 +116,13 @@ class AppTheme {
     primary: AppLightColors.primary,
     onPrimary: AppLightColors.onPrimary,
     backgroundColor: AppColors.paper.shade600,
+    surfaceColor: AppColors.paper,
     textColor: AppLightColors.primary,
     textDisplayColor: AppColors.asphalt,
     dividerColor: AppColors.paper.shade800,
     cardColor: AppColors.paper,
+    inputFillColor: AppColors.paper.shade700,
+    inputEnabledBorderColor: Colors.grey.shade500,
   );
 
   // Dark theme
@@ -71,10 +131,13 @@ class AppTheme {
     primary: AppDarkColors.primary,
     onPrimary: AppDarkColors.onPrimary,
     backgroundColor: AppColors.asphalt.shade50,
+    surfaceColor: AppColors.asphalt.shade100,
     textColor: AppColors.paper,
     textDisplayColor: AppColors.paper,
     dividerColor: AppColors.asphalt,
     cardColor: AppColors.asphalt.shade100,
+    inputFillColor: AppColors.asphalt.shade100,
+    inputEnabledBorderColor: Colors.grey.shade800,
   );
 }
 
